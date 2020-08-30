cffa4f8e73e554eaed3c6e2578db78e237121f1a
Liberty <libertycgjones@gmail.com>
2020-08-30 22:30:58 +0100

    Fixed ball scale issue.

--- Assets/Scripts/BallScaler.cs
+++ Assets/Scripts/BallScaler.cs
@@ -24,11 +24,11 @@ public class BallScaler : MonoBehaviour
 
     void Update()
     {
-        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, speed * Time.deltaTime);
+        //transform.localScale = Vector3.Lerp(transform.localScale, targetScale, speed * Time.deltaTime);
 
         // If you don't want an eased scaling, replace the above line with the following line
         //   and change speed to suit:
-        //transform.localScale = Vector3.MoveTowards (transform.localScale, targetScale, speed * Time.deltaTime);
+        transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, speed * Time.deltaTime);
 
     }
 
@@ -40,7 +40,7 @@ public class BallScaler : MonoBehaviour
         else
             currScale--;
 
-        currScale = Mathf.Clamp(currScale, minSize, maxSize + 1);
+        currScale = Mathf.Clamp(currScale, minSize, maxSize + 0.1f);
 
         targetScale = baseScale * currScale;
     }
@@ -52,6 +52,9 @@ public class BallScaler : MonoBehaviour
             //other.gameObject.SetActive(false);
             Debug.Log("Scale Changed");
             ChangeSize(true);
+            Debug.Log("Position changed");
+            //transform.Translate(0f, -0.5f, 0f);
+            transform.position = new Vector3(transform.position.x, 0.15f, transform.position.z);
         }
     }
 }
