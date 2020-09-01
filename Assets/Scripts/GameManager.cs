f85b0584a53a5d6475e1cd91f957ade8382b892f
Liberty <libertycgjones@gmail.com>
2020-09-01 15:16:05 +0100

    Added an EndTrigger (win condition).

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private bool gameHasEnded = false;
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
 
    // public Animator animator;
 
+    public void WinLevel()
+    {
+        Debug.Log("Level won!");
+        completeLevelUI.SetActive(true);
+    }
+
     public void EndGame()
     {
-       // animator.SetTrigger("EndGame");
+        // animator.SetTrigger("EndGame");
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game over");
+            Invoke("Restart", restartDelay);
+        }
     }
 
     public void RestartLevel()
